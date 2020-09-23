0f815533df13b28f86cd51039785348f507be28d
rugvedmujumdar2007 <70473457+rugvedmujumdar2007@users.noreply.github.com>
2020-09-23 18:51:03 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,16 @@
+class Chain{
+    constructor(bodyA,bodyB){
+        var option={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            stiffness:0.04,
+            length:10
+        }
+        this.chain=Constraint.create(option);
+        World.add(world,this.chain);
+    }
+    display(){
+        strokeWeight(5);
+        line (this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y);
+    }
+}
